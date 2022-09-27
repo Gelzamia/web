166eacae75d2afb7d384eb91144e1ded2fc6c37d
Ибляминова Гельзамия <miss.gelzamia@yandex.ru>
2022-09-28 01:11:47 +0400

    .

--- RequestHandler/src/main/java/com/Web/RequestHandler/RequestHandlerApplication.java
+++ RequestHandler/src/main/java/com/Web/RequestHandler/RequestHandlerApplication.java
@@ -2,13 +2,20 @@ package com.Web.RequestHandler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
+@RestController
+
 public class RequestHandlerApplication {
 
 	public static void main(String[] args) {
-		System.out.println("ghbdtn"); //ааааааааааааааааааааааааааааааааа
+		 //ааааааааааааааааааааааааааааааааа
 		SpringApplication.run(RequestHandlerApplication.class, args);
+	}
+	@GetMapping("/simple-request")
+	public String simpleRequest() {
+		return "ok";
 	}
 
 }
