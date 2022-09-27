a4a4459e147fff62b5c19ca1873747ceb0c2d9fb
Gelzamia <97367859+Gelzamia@users.noreply.github.com>
2022-09-27 23:58:36 +0400

    Add files via upload

---
+++ RequestHandler/src/main/java/com/Web/RequestHandler/RequestHandlerApplication.java
@@ -0,0 +1,13 @@
+package com.Web.RequestHandler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestHandlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RequestHandlerApplication.class, args);
+	}
+
+}
---
+++ RequestHandler/src/test/java/com/Web/RequestHandler/RequestHandlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Web.RequestHandler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequestHandlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
